e2ef0730924a6c4afb34cc03353f212c72badd6a
vassik <Anatoly.Vasilevskiy@sintef.no>
2015-08-19 15:17:48 +0200

    updated example

--- examples/parse_read_create.c
+++ examples/parse_read_create.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #include "cson/cson.h"
 
@@ -15,7 +16,7 @@
 int main(int argc, char* argv[]) {
 
 
-	const char* json_string = "{\"uuid\": \"00a02b93-25d4-4e35-b134-3028a1993c32\", \"command\": {\"default\": {\"value\": \"true\", \"bvalue\": false}}}";
+	const char* json_string = "{\"uuid\": \"00a02b93-25d4-4e35-b134-3028a1993c32\", \"command\": {\"default\": {\"value\": \"true\", \"bvalue\": false, \"intvalue\": 10, \"doublevalue\": 10.1}}}";
 
 	cson_parse_opt opt = cson_parse_opt_empty;
 	cson_parse_info inf = cson_parse_info_empty;
@@ -37,6 +38,8 @@ int main(int argc, char* argv[]) {
 
 	cson_value* command_value = cson_object_get_sub2(obj, ".command.default.value");
 	cson_value* command_value_bool = cson_object_get_sub2(obj, ".command.default.bvalue");
+	cson_value* command_value_int = cson_object_get_sub2(obj, ".command.default.intvalue");
+	cson_value* command_value_double = cson_object_get_sub2(obj, ".command.default.doublevalue");
 
 	if (command_value == NULL) {
 		printf("cannot extract command_value\n");
@@ -48,11 +51,27 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
+	if (command_value_int == NULL) {
+		printf("cannot extract command_value_int\n");
+		return 1;
+	}
+
+	if (command_value_double == NULL) {
+		printf("cannot extract command_value_double\n");
+		return 1;
+	}
+
 	char bool_value = cson_value_get_bool(command_value_bool);
 	printf("command_value_bool=%s\n", bool_value == 1 ? "true" : "false");
 
 	char const * value = cson_value_get_cstr(command_value);
 	printf("command_value=%s\n", value);
+
+	uint8_t value_int = cson_value_get_integer(command_value_int);
+	printf("command_value_int=%d\n", value_int);
+
+	double value_double = cson_value_get_double(command_value_double);
+	printf("command_value_int=%f\n", value_double);
 
 	cson_buffer buf = cson_buffer_empty;
 	rc = cson_output_buffer(root, &buf, NULL);
